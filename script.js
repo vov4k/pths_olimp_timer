7312144d057bae1611ce36f4e01a9c5b53f420e0
vov4k <golovach.volodia@yandex.ru>
2022-09-10 20:46:15 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,69 @@
+let buttonRun = document.getElementById("button");// Берём кнопку запуска
+let timerShow = document.getElementById("timer"); // Берём блок для показа времени
+let startShow = document.getElementById("start"); // Берём блок для показа времени
+let endShow = document.getElementById("end"); // Берём блок для показа времени
+let nowShow = document.getElementById("now"); // Берём блок для показа времени
+let divTime = document.getElementById("time"); // Берём блок для показа времени
+
+Date.prototype.addMinutes = function (h) {
+    this.setSeconds(this.getSeconds() + h);
+    return this;
+}
+
+while (localStorage.getItem("room") === "null" || localStorage.getItem("room") === "" ){
+    localStorage.setItem("room", prompt("Введите номер кабинета"))
+}
+console.log(localStorage.getItem("room"));
+
+function startTimer(timeStart) {
+    timeSeconds = timeStart;
+    const date = new Date();
+
+    const requestURL = `/start_olympiad?room=${localStorage.getItem("room")}&duration=${timeStart}`;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', requestURL);
+    xhr.send();
+
+    startShow.innerHTML = `${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}`;
+    date.addMinutes(timeSeconds);
+    endShow.innerHTML = `${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}`;
+
+    divTime.style.display = "";
+    buttonRun.style.display = "none";
+
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    }
+}
+
+buttonRun.addEventListener('click', function () {
+    startTimer(5400);
+})
+
+timer = setInterval(function () {
+    let date;
+    seconds = timeSeconds % 60 // Получаем секунды
+    minutes = timeSeconds / 60 % 60 // Получаем минуты
+    hour = timeSeconds / 60 / 60 % 60 // Получаем часы
+
+    if (timeSeconds <= 0) {
+        clearInterval(timer);
+    } else if (window.location.href.slice(-7, -6) === 's' && timeSeconds <= 1800) {
+        let table = document.getElementsByClassName("f_table"); // Берём блок для показа времени
+        table = table[0];
+        table.innerHTML = `<img src="/frame.png" width="600">`;
+
+        timerShow.innerHTML = `${("0" + Math.trunc(hour)).slice(-2)}:${("0" + Math.trunc(minutes)).slice(-2)}`;
+        date = new Date();
+        nowShow.innerHTML = `${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}`;
+
+    } else {
+        timerShow.innerHTML = `${("0" + Math.trunc(hour)).slice(-2)}:${("0" + Math.trunc(minutes)).slice(-2)}`;
+        date = new Date();
+        nowShow.innerHTML = `${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}`;
+    }
+    --timeSeconds; // Уменьшаем таймер
+
+}, 1000)
+
+
